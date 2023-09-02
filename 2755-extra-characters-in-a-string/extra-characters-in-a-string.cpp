d32b6c583a4857ee6033dcfe0f2bd28bb16fe957
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-09-02 22:22:14 +0530

    Time: 381 ms (22.57%) | Memory: 206.6 MB (13.81%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,32 +1,27 @@
 class Solution {
-    int f(int ind,string s,unordered_map<string,bool>&mp,vector<int> &dp){
-        if(ind>=s.size()){
-            return 0;
-        }
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-        string curr="";
-        int res=s.size();
-        for(int i=ind;i<s.size();i++){
-            curr.push_back(s[i]);
-            int extra=curr.size();
-            if(mp[curr]){
-                extra=0;
-            }
-            extra+=f(i+1,s,mp,dp);
-            res=min(res,extra);
-        }
-        return dp[ind]=res;
-    }
 public:
     int minExtraChar(string s, vector<string>& dictionary) {
         unordered_map<string,bool> mp;
-        vector<int> dp(s.size(),-1);
+        int n=s.size();
+        vector<int> dp(s.size()+1,0);
         for(auto&word :dictionary){
             mp[word]=true;
         }
-        return f(0,s,mp,dp);
+        for(int ind=n-1;ind>=0;ind--){
+            string curr="";
+            int res=s.size();
+            for(int i=ind;i<s.size();i++){
+                curr.push_back(s[i]);
+                int extra=curr.size();
+                if(mp[curr]){
+                    extra=0;
+                }
+                extra+=dp[i+1];
+                res=min(res,extra);
+            }
+             dp[ind]=res;
+        }
+        return dp[0];
         
     }
 };
