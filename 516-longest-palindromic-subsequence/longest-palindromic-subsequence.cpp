119b44036f0c0d143b8457037fe21d01a5f7a7d0
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-09-05 21:39:18 +0530

    Time: 107 ms (64.20%) | Memory: 6.7 MB (92.70%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -16,25 +16,21 @@ public:
         string s2=s;
         reverse(s.begin(),s.end());
         int n=s.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        for(int j=0;j<=n;j++){
-            dp[0][j]=0;
-        }
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-        }
+        vector<int> prev(n+1,0),cur(n+1,0);
+        
         for(int ind1=1;ind1<=n;ind1++){
             for(int ind2=1;ind2<=n;ind2++){
                  if(s[ind1-1]==s2[ind2-1]){
-                  dp[ind1][ind2]= 1+dp[ind1-1][ind2-1];
+                  cur[ind2]= 1+prev[ind2-1];
                }
                else{
-                    dp[ind1][ind2]=max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                    cur[ind2]=max(prev[ind2],cur[ind2-1]);
                }
               
             }
+            prev=cur;
         }
-        return dp[n][n];
+        return prev[n];
 
     }
 };
