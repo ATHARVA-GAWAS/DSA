3cb4bba71ccce45fad0778d508ec800cd3863b08
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-09-08 22:51:11 +0530

    Time: 162 ms (80.05%) | Memory: 58.9 MB (49.61%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -23,21 +23,40 @@ bool dfs(int node, vector<int>& vis,vector<int>& pathvis, vector<vector<int>>& g
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<int> vis(n,0);
-        vector<int> pathvis(n,0);
-        vector<int> check(n,0);
+        
+         
+         vector<int> adjRev[n];
+          vector<int> indegree(n,0);
+
+        //create reversed adjacency list
+        for(int i=0;i<n;i++){
+            for(auto it:graph[i]){
+                adjRev[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+        //push terminal nodes in the queue
+        queue<int> q;
         vector<int> safenodes;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,vis,pathvis,graph,check);
+            if(indegree[i]==0){
+                q.push(i);
             }
         }
-        for(int i=0;i<n;i++){
-            if(check[i]==1){
-                safenodes.push_back(i);
+        //BFS
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            safenodes.push_back(node);
+            for(auto it: adjRev[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
             }
+            
         }
-        
+        sort(safenodes.begin(),safenodes.end());
         return safenodes;
     }
 };
