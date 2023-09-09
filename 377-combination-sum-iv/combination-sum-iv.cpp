8bb761760f4eda3c73861f991f3885381992a592
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-09-09 17:11:52 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (46.24%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
-       int f(int n, vector<int>& nums, int target, vector<int>& dp) {
-
-        if (target == 0) {
-            return 1;
-        }
-        if (target < 0) {
-            return 0;
-        }
-        if (dp[target] != -1) {
-            return dp[target];
-        }
-        int take = 0;
-        for (int j = 0; j < n; ++j) {
-            take += f(n, nums, target - nums[j], dp); 
-        }
-        return dp[target] = take;
-    }
- 
-    int combinationSum4(vector<int>& nums, int target) {
-        vector<int>dp(target+1,-1);
-          return f(nums.size(),nums,target,dp);
+  int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> dp(target+1, 0);
+        dp[0]=1;
+        for(int i=1;i<=target;i++){
+            for(int j=0;j<nums.size();j++){
+                if(i-nums[j]>=0)
+                    dp[i]+=dp[i-nums[j]];
+             }
+         }
+        return dp[target];
     }
 };
