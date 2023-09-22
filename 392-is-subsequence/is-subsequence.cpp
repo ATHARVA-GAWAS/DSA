c9231bf30bef82c846f38e1848cb77331285b4d0
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-09-22 20:18:36 +0530

    Time: 4 ms (10.63%) | Memory: 8.6 MB (5.02%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,22 +1,20 @@
 class Solution {
-    int f(int ind1,int ind2,string s,string t,vector<vector<int>> &dp){
-        if(ind1<0 || ind2<0){
-            return 0;
-        }
-        if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
-        }
-        if(s[ind1]==t[ind2]){
-            return dp[ind1][ind2]=(1+f(ind1-1,ind2-1,s,t,dp));
-        }
-        return dp[ind1][ind2]=max(f(ind1-1,ind2,s,t,dp),f(ind1,ind2-1,s,t,dp));
-    }
 public:
     bool isSubsequence(string s, string t) {
         int n1=s.size();
         int n2=t.size();
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
-         int ans=f(n1-1,n2-1,s,t,dp);
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        for(int ind1=1;ind1<=n1;ind1++){
+            for(int ind2 = 1;ind2<=n2;ind2++){
+                 if(s[ind1-1]==t[ind2-1]){
+                     dp[ind1][ind2]=(1+dp[ind1-1][ind2-1]);
+                 }
+                 else{
+                     dp[ind1][ind2]=max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                 }
+            }
+        }
+         int ans=dp[n1][n2];
          if(ans==n1){
              return true;
          }
