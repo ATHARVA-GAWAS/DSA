df1b522ca9cb0fd582b0a781d540e69397bab1b9
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-09-25 23:51:20 +0530

    Time: 8 ms (99.13%) | Memory: 13.3 MB (83.24%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -7,10 +7,8 @@ public:
         vector<int>indegree(n,0);
         vector<int>adj[n];
         for(int i=0; i<m; i++){
-            int node1= graph[i][0];
-            int node2= graph[i][1];
-            adj[node2].push_back(node1);
-            indegree[node1]++;
+            adj[graph[i][1]].push_back(graph[i][0]);
+            indegree[graph[i][0]]++;
             
         }
         queue<int>q;
@@ -21,15 +19,15 @@ public:
         }
         vector<int>ans;
         while(!q.empty()){
-            int node= q.front();
-            ans.push_back(node);
-            q.pop();
-            for(auto it:adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
-                }
-            }
+           int node=q.front();
+           ans.push_back(node);
+           q.pop();
+           for(auto it: adj[node]){
+               indegree[it]--;
+               if(indegree[it]==0){
+                   q.push(it);
+               }
+           }
         }
         if(ans.size()==n) return true;
         return false;
