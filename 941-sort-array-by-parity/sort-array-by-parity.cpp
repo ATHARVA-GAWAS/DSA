36e1e366907b0e269915b934a25bd228447833e8
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-09-28 16:54:47 +0530

    Time: 3 ms (96.18%) | Memory: 16.8 MB (8.80%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -6,11 +6,10 @@ public:
         for(int i=0;i<n;i++){
             if(nums[i]%2==0){
                 ans.push_back(nums[i]);
-                nums[i]=-1;
             }
         }
         for(int i=0;i<n;i++){
-            if(nums[i]!=-1){
+            if(nums[i]%2!=0){
                 ans.push_back(nums[i]);
             }
         }
