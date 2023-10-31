976319db89441abdbb09aba59ea954129e43ff0c
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-10-31 16:31:41 +0530

    Time: 83 ms (72.85%) | Memory: 76.2 MB (83.50%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     vector<int> findArray(vector<int>& pref) {
         int n=pref.size();
-        vector<int> ans;
-        ans.push_back(pref[0]);
-        for(int i=1;i<n;i++){
-            ans.push_back(pref[i]^pref[i-1]);
+        
+        for(int i=n-1;i>0;i--){
+            pref[i]=(pref[i]^pref[i-1]);
         }
-        return ans;
+        return pref;
     }
 };
