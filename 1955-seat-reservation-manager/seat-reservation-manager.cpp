13d534e8c01b0190f41fc1c61275a3437166fbd9
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-11-06 19:32:52 +0530

    Time: 281 ms (89.53%) | Memory: 141.8 MB (98.85%) - LeetSync

--- 1955-seat-reservation-manager/seat-reservation-manager.cpp
+++ 1955-seat-reservation-manager/seat-reservation-manager.cpp
@@ -1,15 +1,19 @@
 class SeatManager {
 public:
 priority_queue<int,vector<int>,greater<int>>pq;
+int seat_marker;
     SeatManager(int n) {
-        for(int i=1;i<=n;i++){     //n
-            pq.push(i);   //logn
-        }
+        seat_marker=1;
     }
     
     int reserve() {
-        int seat=pq.top();
-        pq.pop();
+        if(!pq.empty()){
+            int seat=pq.top();
+            pq.pop();
+            return seat;
+        }
+        int seat=seat_marker;
+        seat_marker++;
         return seat;
     }
     
