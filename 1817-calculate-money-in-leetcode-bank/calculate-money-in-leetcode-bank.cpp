f8818b42eb438bd42f405d56bc43d08297571a1f
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-12-06 19:34:12 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (98.95%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int result=0;
-        int monday_money=1;
-        while(n>0){
-            int money=monday_money;
-            for(int day=1;day<=min(n,7);day++){
-                result+=money;
-                money++;
-            }
-            n-=7;
-            monday_money++;
+        int items  = n / 7;
+        int first  = 28;
+        int last   = 28 + (items - 1) * 7; //Tn = a1 + (n-1)*d
+        
+        int arithmeticSum = items * (first + last) / 2; //Sn = n/2(a1 + an)
+        
+        //Final week remaining days
+        int monday_money = 1 + items;
+        int finalWeek    = 0;
+        
+        for (int day = 1; day <= (n % 7); day++) { //This loop will Never iterate more than 6 ~ O(1)
+            finalWeek += monday_money++;
         }
-        return result;
+        
+        return arithmeticSum + finalWeek;
     }
 };
