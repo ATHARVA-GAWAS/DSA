cc85f4f4f5d155a392daa5711285fcc86dbaadcc
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-12-11 15:58:48 +0530

    Time: 8 ms (78.76%) | Memory: 12.8 MB (57.06%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
@@ -1,14 +1,65 @@
 class Solution {
 public:
-    int findSpecialInteger(vector<int>& arr) {
-        int n=arr.size();
-        int m=25*n/100;
-        for(int i=0;i<n-m;i++){
+        int firstOccur(int target, int l, int r, vector<int>& arr) {
+        
+        int result = -1;
+        
+        while(l <= r) {
+            int mid = l + (r-l)/2;
             
-            if(arr[i]==arr[i+m]){
-                return arr[i];
+            if(arr[mid] == target) {
+                result = mid;
+                r = mid-1;
+            } else if(arr[mid] < target) {
+                l = mid+1;
+            } else {
+                r = mid-1;
             }
         }
+        
+        return result;
+        
+    }
+    
+    int lastOccur(int target, int l, int r, vector<int>& arr) {
+        
+        int result = -1;
+        
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            
+            if(arr[mid] == target) {
+                result = mid;
+                l = mid+1;
+            } else if(arr[mid] < target) {
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+        
+        return result;
+        
+    }
+    
+    int findSpecialInteger(vector<int>& arr) {
+        int n = arr.size();
+        
+        int freq = n/4;
+        
+        vector<int> candidates{arr[n/4], arr[n/2], arr[3*n/4]};
+        
+        for(int &candidate : candidates) {
+            
+            int left = firstOccur(candidate, 0, n-1, arr);
+            int right = lastOccur(candidate, 0, n-1, arr);
+            
+            
+            if(right-left+1 > freq) {
+                return candidate;
+            }
+        }
+        
         return -1;
         
     }
