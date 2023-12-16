c72a8bce8ded4057fbbc652170090ab19669ee50
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-12-16 16:28:27 +0530

    Time: 3 ms (97.78%) | Memory: 7.6 MB (98.01%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        int n=s.length();
+        int m=t.length();
+        if(n!=m){
+            return false;
+        }
+        int map[26]={0};
+        for(int i=0;i<n;i++){
+            map[s[i]-'a']++;
+            map[t[i]-'a']--;
+        }
+        for(int i:map){
+            if(i!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
