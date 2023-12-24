6f465be106b56818a4a9887cfd12a91731622519
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-12-24 11:17:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (86.65%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -4,25 +4,18 @@ public:
         int n=s.size();
         int start_with_0=0;  //010101
         int start_with_1=0;  //101010
-        for(int i=0;i<n;i++){
-            if(i%2==0){
-                
-                if(s[i]=='0'){
-                    start_with_1++;
-                }
-                else {
+        for(int i = 0; i<n; i++) {
+            if(i%2 == 0) {
+                if(s[i] == '1') {
                     start_with_0++;
                 }
-            }
-            else{
-                if(s[i]=='1'){
-                    start_with_1++;
-                }
-                else{
+            } else {
+                if(s[i] == '0') {
                     start_with_0++;
                 }
             }
         }
-        return min(start_with_1,start_with_0);
+        
+        return min(start_with_0, n - start_with_0);
     }
 };
