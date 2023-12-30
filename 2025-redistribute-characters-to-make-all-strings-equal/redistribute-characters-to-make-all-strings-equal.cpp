c561928dba2e782af20ebd83a4669ef404e1183b
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-12-30 14:59:00 +0530

    Time: 22 ms (12.84%) | Memory: 11.8 MB (99.61%) - LeetSync

---
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+       int n=words.size();
+        
+       int count[26]={0};
+        for(string &word :words){
+            for(char &ch :word){
+                count[ch-'a']++;
+            }
+        }
+        
+        for(auto &freq: count){
+            
+            if(freq%n!=0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
