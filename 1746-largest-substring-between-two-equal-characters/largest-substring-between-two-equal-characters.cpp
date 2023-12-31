89addb41ce54a6ef942302bf267b73682ad0b2de
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2023-12-31 12:16:49 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (41.15%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
         int n=s.size();
+        unordered_map<char,int> mp;
         int maxdiff=-1;
         for(int i=0;i<n;i++){
             int diff=0;
-            for(int j=n-1;j>i;j--){
-                if(s[i]==s[j]){
-                    diff= j-i-1;
-                    maxdiff=max(maxdiff,diff);
-                }
+            char ch=s[i];
+            if(mp.find(ch)==mp.end()){
+                mp[ch]=i;
             }
+            else{
+                diff= i-mp[ch]-1;
+                maxdiff=max(maxdiff,diff);
+            }
+            
         }
         return maxdiff;
     }
 };
+// tc: O(N)  sc= O(26)
