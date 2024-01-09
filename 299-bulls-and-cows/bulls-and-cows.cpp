4f8fa87d5085afcb6dacca6aa617e446b5783d70
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-01-09 22:25:32 +0530

    Time: 6 ms (39.73%) | Memory: 7 MB (43.08%) - LeetSync

---
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string getHint(string& secret, string &guess) {
+        unordered_map<char,int> secret_freq;
+        unordered_map<char,int> guess_freq;
+
+        int n = secret.size();
+        int bulls = 0;
+        int cows = 0;
+        for(int i=0;i<n;i++){
+            if(secret[i] == guess[i]){
+                bulls++;
+            }else{
+                secret_freq[secret[i]]++;
+                guess_freq[guess[i]]++;
+            }
+        }
+        for(auto it: guess_freq){
+            cows += min(it.second,secret_freq[it.first]);
+        }
+        return to_string(bulls)+"A"+to_string(cows)+"B";
+    }
+};
