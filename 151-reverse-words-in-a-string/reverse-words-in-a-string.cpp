ae65a1dc1811b93b5bd33b4fa54de08872d1d879
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-03-06 09:00:46 +0530

    Time: 3 ms (83.98%) | Memory: 8.5 MB (76.22%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -4,11 +4,12 @@ public:
         int n=s.size();
         string temp="";
         string ans="";
+        //reverse traversal
         for(int i=n-1;i>=0;i--){
-            if(s[i]!=' '){
+            if(s[i]!=' '){         // skip spaces and include letters only to temp string inorder to get words (since we are traversing in the reverse order, we get reversed words)
                 temp+=s[i];
             }
-            if(temp.size()>0 && s[i]==' '){
+            if(temp.size()>0 && s[i]==' '){             //if we encounter space between words, we reverse the words and put them in ans string
                 reverse(temp.begin(),temp.end());
                 ans+=temp;
                 temp="";
@@ -18,7 +19,7 @@ public:
             }
         }
        
-        reverse(temp.begin(),temp.end());
+        reverse(temp.begin(),temp.end());    //handle first word of the string that doesnt get included during above process
         ans+=temp;
         if(ans[ans.size()-1]==' '){
             ans.erase(ans.size()-1);
