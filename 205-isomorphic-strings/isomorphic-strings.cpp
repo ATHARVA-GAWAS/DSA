1a3f361dc17c74502f3fc67a0d04b65f030c0138
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-04-02 17:30:50 +0530

    Time: 6 ms (65.28%) | Memory: 8.2 MB (85.95%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<int> indS(200,0);
+        vector<int> indT(200,0);
+
+        int n=s.length();
+
+        if(n!=t.length()){
+            return false;
+        }
+
+        for(int i=0;i<n;i++){
+            if(indS[s[i]]!=indT[t[i]]){
+                return false;
+            }
+
+            indS[s[i]]=i+1;
+            indT[t[i]]=i+1;
+        }
+        return true;
+    }
+};
