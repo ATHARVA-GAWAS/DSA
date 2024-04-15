5c4907fb00758b830e2ca812a5d5d846c0be6099
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-04-15 21:33:01 +0530

    Time: 8 ms (27.51%) | Memory: 11.2 MB (61.63%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -14,7 +14,20 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         int m=triangle[0].size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return f(0,0,triangle,n,dp);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int j=0;j<n;j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                if(i==n-1){
+                    dp[i][j]= triangle[n-1][j];
+                }
+                int d=triangle[i][j]+dp[i+1][j];
+                int dg=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(d,dg);
+            }
+        }
+       return dp[0][0];
     }
 };
