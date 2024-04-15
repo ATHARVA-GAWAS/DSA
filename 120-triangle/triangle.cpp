6fe48d64bc35220209ad8a9a534002a5b9187470
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-04-15 21:26:29 +0530

    Time: 4 ms (73.38%) | Memory: 11.4 MB (26.34%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,24 +1,20 @@
 class Solution {
+    int f(int i, int j,vector<vector<int>>& triangle, int n,vector<vector<int>> &dp){
+        if(i==n-1){
+            return triangle[n-1][j];
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int d=triangle[i][j]+f(i+1,j,triangle,n,dp);
+        int dg=triangle[i][j]+f(i+1,j+1,triangle,n,dp);
+        return dp[i][j]=min(d,dg);
+    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        int m=triangle.size();
-        vector<int> after(n+1,0),cur(m+1,0);
-        for(int j=0;j<n;j++){
-            after[j] = triangle[n-1][j];
-        }
-        for(int i=n-2;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                     if(i==n-1){
-                            cur[j]=triangle[n-1][j];
-                        }
-                        
-                    int d=triangle[i][j]+after[j];
-                    int dg=triangle[i][j]+after[j+1];
-                    cur[j]=min(d,dg);
-                }
-                after=cur;
-        }
-        return after[0];
+        int m=triangle[0].size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return f(0,0,triangle,n,dp);
     }
 };
