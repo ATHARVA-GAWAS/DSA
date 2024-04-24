ac3f5a33b530c6f4f6e3b8d2306c4bd34dbd3579
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-04-24 23:44:55 +0530

    Time: 17 ms (49.89%) | Memory: 16.7 MB (95.59%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& graph) {
-         int m= graph.size();
-        vector<int>indegree(n,0);
-        vector<int>adj[n];
-        for(int i=0; i<m; i++){
-            int node1= graph[i][0];
-            int node2= graph[i][1];
-            adj[node2].push_back(node1);
-            indegree[node1]++;
-            
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int m=prerequisites.size();
+        int n=numCourses;
+        vector<int> adj[n];
+        vector<int> indegree(n,0);
+
+        for(int i=0;i<m;i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
         }
-        queue<int>q;
-        for(int i=0; i<n; i++){
+
+        queue<int> q;
+
+        for(int i=0;i<n;i++){
             if(indegree[i]==0){
                 q.push(i);
             }
+
         }
-        vector<int>ans;
+        vector<int> ans;
+        
         while(!q.empty()){
-            int node= q.front();
-            ans.push_back(node);
+            int node=q.front();
             q.pop();
-            for(auto it:adj[node]){
+
+            ans.push_back(node);
+
+            for(auto it: adj[node]){
                 indegree[it]--;
                 if(indegree[it]==0){
                     q.push(it);
                 }
             }
         }
-        if(ans.size()==n) return ans;
+
+        if(ans.size()==n){
+            return ans;
+        }
+        
         return {};
+        
     }
-   
 };
