1f065a33a04b22bffde14a25a240021af24d1c8f
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-04-29 08:39:41 +0530

    Time: 115 ms (42.41%) | Memory: 91.4 MB (25.13%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int xorResult = k;
-        for (int num : nums) {
-            xorResult ^= num;
-        }
-        int count = 0;
-        while (xorResult > 0) {
-            count += xorResult & 1;
-            xorResult =xorResult >> 1;
+        int x=0, res=0;
+        // Calculate the XOR of all elements in the array
+        for(int& a:nums)
+            x ^= a;
+        
+        // Iterate until both x and k become 0
+        while(x || k){
+            // Check if the least significant bits of x and k are different
+            if((x&1) != (k&1))
+                res++;
+            
+            // Right shift x and k to check the next bit
+            x >>= 1;
+            k >>= 1;
         }
 
-        return count;
+        return res;
     }
 };
