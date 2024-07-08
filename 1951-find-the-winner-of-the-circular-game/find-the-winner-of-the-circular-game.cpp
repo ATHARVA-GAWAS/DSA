4b2568bc95a7c482fdf15017f777aa477883fe30
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-07-08 09:06:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (76.96%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,14 +1,14 @@
 class Solution {
-public:
-    int solve(int n, int k){
-        if(n == 1)
-        return 0;
-
-        return (solve(n-1, k) + k)%n;
+    int f(int n, int k){
+        if(n == 1){
+            return 0;
+        }
+        return (f(n-1, k) + k)%n;
     }
+public:
     int findTheWinner(int n, int k) {
         
-        int ans = solve(n, k) + 1;
+        int ans = f(n, k) + 1;
         return ans;
     }
 };
