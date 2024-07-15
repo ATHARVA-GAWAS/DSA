030f92bb62115ef09c50c12703852ff2747cfbd7
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-07-15 16:03:52 +0530

    Time: 47 ms (47.22%) | Memory: 51 MB (25.71%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxPosition = 0;
-        int currentPosition = 0;
-
-        while (currentPosition <= maxPosition && maxPosition < nums.size() - 1)
-        {
-            maxPosition = max(maxPosition, currentPosition + nums[currentPosition]);
-            currentPosition += 1;
+        int maxInd=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>maxInd){
+                return false;
+            }
+            maxInd=max(maxInd,i+nums[i]);
         }
-        
-        return maxPosition < nums.size() - 1 ? false : true;
+        return true;
     }
 };
