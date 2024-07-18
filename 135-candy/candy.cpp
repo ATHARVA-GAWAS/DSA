9401531702cd04ea059d5feefc7cfd5b12e06ff8
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-07-18 22:16:39 +0530

    Time: 17 ms (34.47%) | Memory: 20.4 MB (22.63%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-         int n = ratings.size();
-        std::vector<int> candies(n, 1);
+        int n = ratings.size();
+        vector<int> candies(n, 1);
 
+        //left neighbour 
         for (int i = 1; i < n; ++i) {
             if (ratings[i] > ratings[i - 1]) {
                 candies[i] = candies[i - 1] + 1;
             }
         }
 
+        //right neighbour
         for (int i = n - 2; i >= 0; --i) {
             if (ratings[i] > ratings[i + 1]) {
-                candies[i] = std::max(candies[i], candies[i + 1] + 1);
+                candies[i] = max(candies[i], candies[i + 1] + 1);
             }
         }
 
