f4139845108a5d4691f392539d1230194a3576d8
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-07-19 01:14:54 +0530

    Time: 7 ms (61.27%) | Memory: 12.5 MB (66.07%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
+
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int> > dp(n,vector<int>(m,0));
-        for(int i=0; i<n ; i++){
-            for(int j=0; j<m; j++){
-                if(i==0 && j==0) dp[i][j] = grid[i][j];
-                else{
-                    int up = grid[i][j];
-                    if(i>0) up += dp[i-1][j];
-                    else up += 1e9;
-                    
-                    int left = grid[i][j];
-                    if(j>0) left+=dp[i][j-1];
-                    else left += 1e9;
-                    
-                    dp[i][j] = min(up,left);
-                }
+        vector<int> prev(n+1,0),cur(m+1,0);
+    for(int i=0; i<n ; i++){
+        for(int j=0; j<m; j++){
+            if(i==0 && j==0) cur[j] = grid[i][j];
+            else{
+                
+                int up = grid[i][j];
+                if(i>0) up += prev[j];
+                else up += 1e9;
+                
+                int left = grid[i][j];
+                if(j>0) left+=cur[j-1];
+                else left += 1e9;
+                
+                cur[j] = min(up,left);
             }
         }
-        return dp[n-1][m-1];
+        prev=cur;
+    }
+    
+    return prev[m-1];
     }
 };
