6be1970fe071e8c8578da64f35c9c96551ee9eaf
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-07-29 21:25:26 +0530

    Time: 19 ms (94.73%) | Memory: 11.4 MB (63.96%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -3,14 +3,28 @@ public:
     int numTeams(vector<int>& rating) {
         int count = 0;
         int n = rating.size();
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if ((rating[i] < rating[j] && rating[j] < rating[k]) ||(rating[i] > rating[j] && rating[j] > rating[k])) {
-                        count++;
-                    }
+
+        for(int k=1;k<n-1;k++){
+            int cntSmallLeft=0,cntLargeLeft=0,cntSmallRight=0,cntLargeRight=0;
+
+            for(int i=0;i<k;i++){
+                if(rating[i]<rating[k]){
+                    cntSmallLeft++;
+                }
+                else if(rating[i]>rating[k]){
+                    cntLargeLeft++;
                 }
             }
+
+            for(int j=k+1;j<n;j++){
+                if(rating[k]<rating[j]){
+                    cntLargeRight++;
+                }
+                else if(rating[k]>rating[j]){
+                    cntSmallRight++;
+                }
+            }
+            count+=(cntLargeLeft*cntSmallRight)+(cntSmallLeft*cntLargeRight);
         }
         return count;
     }
