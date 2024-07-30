e5245f7e93a7a6b92a93b2de64fa53eda3c3a15c
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-07-30 19:45:31 +0530

    Time: 49 ms (17.88%) | Memory: 58.2 MB (21.39%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -7,17 +7,14 @@ public:
             if(nums[r]==0){
                 zeroes++;
             }
-            if(zeroes>k){
+            while(zeroes>k){
                 if(nums[l]==0){
                     zeroes--;
                 }
                 l++;
             }
-
-            if(zeroes<=k){
-                int len=r-l+1;
-                maxlen=max(maxlen,len);
-            }
+            int len=r-l+1;
+            maxlen=max(maxlen,len);
             r++;
         }
         return maxlen;
