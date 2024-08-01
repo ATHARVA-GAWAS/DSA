b5b48ad8e04ae5016242a2218fdccbe68de863cc
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-08-01 14:52:53 +0530

    Time: 6 ms (77.91%) | Memory: 8.4 MB (58.49%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -3,19 +3,24 @@ public:
     int characterReplacement(string s, int k) {
         int n=s.size(),r=0,l=0,maxlen=0,maxf=0;
         vector<int> mp(26,0);
+
         while(r<n){
             mp[s[r]-'A']++;
             maxf=max(maxf,mp[s[r]-'A']);
 
             if((r-l+1)-maxf >k){
                 mp[s[l]-'A']--;
+                maxf=0;
+
+                for(int i=0;i<26;i++){
+                    maxf=max(maxf,mp[i]);
+                }
                 l++;
             }
 
             if((r-l+1)-maxf <=k){
                 maxlen=max(maxlen,r-l+1);
             }
-
             r++;
         }
         return maxlen;
