70512da512bffd5914cf5fa70ad1002090e20fc3
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-09-11 13:41:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (44.87%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        bitset<32> a(start);
-        bitset<32> b(goal);
+        int res;
+        res=start^goal;
 
-        string a1=a.to_string();
-        string b1=b.to_string();
-
+        bitset<32> a(res);
+        string s=a.to_string();
         int ans=0;
 
         for(int i=0;i<32;i++){
-            if(a1[i]!=b1[i]){
+            if(s[i]=='1'){
                 ans++;
             }
         }
