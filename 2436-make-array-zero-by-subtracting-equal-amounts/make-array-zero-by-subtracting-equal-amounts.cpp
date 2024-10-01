fb61514105608488be85b41858a2c9e9b5464758
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-10-01 08:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (18.94%) - LeetSync

---
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        priority_queue <int, vector<int>, greater<int> > pq;
+        
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+        }
+            
+        int curr_min=0;
+        int count=0;
+        
+        while(!pq.empty()){
+            if(pq.top()==0){
+                pq.pop();
+            }
+            else{
+                int top=pq.top()-curr_min;
+                if(top!=0){
+                    curr_min+=top;
+                    count++;
+                }
+                pq.pop();
+            }
+        }
+        return count;
+    }
+};
