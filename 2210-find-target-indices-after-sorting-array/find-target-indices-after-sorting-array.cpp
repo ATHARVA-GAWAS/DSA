e1541978fd484a907af9eeb8e5d2474cbbd26cf8
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-10-12 23:42:04 +0530

    Time: 4 ms (64.39%) | Memory: 14.5 MB (80.61%) - LeetSync

---
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> targetIndices(std::vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+
+        vector<int> ans;
+
+        int end;
+        int start = search(nums, target, true);
+
+        if (start == -1) {
+            return ans;
+        } else {
+            end = search(nums, target, false);
+        }
+
+        for (int i = start; i <= end; i++) {
+            ans.push_back(i);
+        }
+        return ans;
+    }
+
+private:
+    int search(std::vector<int>& nums, int target, bool isFirstIndex) {
+        int ans = -1;
+        int first = 0;
+        int last = nums.size() - 1;
+
+        while (first <= last) {
+            int mid = first + (last - first) / 2;
+            if (target > nums[mid]) {
+                first = mid + 1;
+            } else if (target < nums[mid]) {
+                last = mid - 1;
+            } else {
+                ans = mid;
+                if (isFirstIndex) {
+                    last = mid - 1;
+                } else {
+                    first = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
