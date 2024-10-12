69fef86f3a94c76fd41cb32b584738c528b611d3
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-10-12 23:43:10 +0530

    Time: 3 ms (76.68%) | Memory: 14.6 MB (10.11%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -1,26 +1,4 @@
 class Solution {
-public:
-    vector<int> targetIndices(std::vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-
-        vector<int> ans;
-
-        int end;
-        int start = search(nums, target, true);
-
-        if (start == -1) {
-            return ans;
-        } else {
-            end = search(nums, target, false);
-        }
-
-        for (int i = start; i <= end; i++) {
-            ans.push_back(i);
-        }
-        return ans;
-    }
-
-private:
     int search(std::vector<int>& nums, int target, bool isFirstIndex) {
         int ans = -1;
         int first = 0;
@@ -40,6 +18,26 @@ private:
                     first = mid + 1;
                 }
             }
+        }
+        return ans;
+    }
+public:
+    vector<int> targetIndices(std::vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+
+        vector<int> ans;
+
+        int end;
+        int start = search(nums, target, true);
+
+        if (start == -1) {
+            return ans;
+        } else {
+            end = search(nums, target, false);
+        }
+
+        for (int i = start; i <= end; i++) {
+            ans.push_back(i);
         }
         return ans;
     }
