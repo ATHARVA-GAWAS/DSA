f0c6a59f2b1296e712c86e5fba90133f56a50e0a
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-11-25 22:39:05 +0530

    Time: 94 ms (26.41%) | Memory: 47.4 MB (36.13%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,35 +1,47 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> adj(n+1,vector<pair<int,int>>{} );
-        for(auto x:times){
-            adj[x[0]].push_back({x[1],x[2]});
+        
+        vector<pair<int,int>> adj[n+1];
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+
+        for(auto it:times){
+            adj[it[0]].push_back({it[1],it[2]});
         }
 
         vector<int> dist(n+1,1e9);
+
         dist[k]=0;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         pq.push({0,k});
 
+        
         while(!pq.empty()){
             int node=pq.top().second;
-            int dis=pq.top().first;
+            int distance=pq.top().first;
             pq.pop();
 
             for(auto x:adj[node]){
-                int adjNode=x.first;
-                int adjDist=x.second;
-                if(dis+adjDist < dist[adjNode]){
-                    dist[adjNode]=dis+adjDist;
-                    pq.push({adjDist+dis,adjNode});
+                int adjnode=x.first;
+                int adjdist=x.second;
+
+                if(distance+adjdist < dist[adjnode]){
+                    dist[adjnode]=distance+adjdist;
+                    pq.push({adjdist+distance, adjnode});
                 }
             }
         }
+
         int ans=-1;
+
         for(int i=1;i<=n;i++){
             ans=max(ans,dist[i]);
         }
-        if(ans==1e9){return -1;}
+
+        if(ans==1e9){
+            return -1;
+        }
+
         return ans;
     }
 };
