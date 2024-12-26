6fe057468ad78941ff1465cca93a566652c674c0
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-12-26 11:38:20 +0530

    Time: 159 ms (44.88%) | Memory: 11.5 MB (74.02%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,40 +1,45 @@
 class Solution {
-    int f(int ind,int target,vector<int>& arr,vector<vector<int>>& dp){
-        if(ind == 0){
-        if(target==0 && arr[0]==0){
-               return 2;
+    int f(int ind,int target,vector<int>&nums){
+
+        if(ind==0){
+            if(nums[ind]==target && target==0){
+                return 2;
+            }
+            if(nums[ind]==target || target==0){
+                return 1;
+            }
+           
+            return 0;
         }
-            
-        if(target==0 || target == arr[0]){
-               return 1;
-           }
-            
-        return 0;
-    }
-    if(dp[ind][target]!=-1){
-        return dp[ind][target];
-    }
-    int notTaken = f(ind-1,target,arr,dp);
-    
-    int taken = 0;
-    if(arr[ind]<=target)
-        taken = f(ind-1,target-arr[ind],arr,dp);
+
+        int nottake=f(ind-1,target,nums);
+
+        int take=0;
         
-    return  dp[ind][target]=(notTaken + taken);
+        if(nums[ind]<=target){
+            take=f(ind-1,target-nums[ind],nums);
+        }
+        
+        return take+nottake;
     }
 public:
-    int findTargetSumWays(vector<int>& arr, int target) {
+    int findTargetSumWays(vector<int>& nums, int target) {
         int totSum = 0;
-        int n=arr.size();
-    for(int i=0; i<arr.size();i++){
-        totSum += arr[i];
-    }
-    if(totSum-target<0) return 0;
-    if((totSum-target)%2==1) return 0;
-    
-    int s2 = (totSum-target)/2;
-    
-    vector<vector<int>> dp(n,vector<int>(s2+1,-1));
-    return f(n-1,s2,arr,dp);
+        int n=nums.size();
+
+        for(int i=0; i<nums.size();i++){
+            totSum += nums[i];
+        }
+
+        if(totSum-target<0){
+            return 0;
+        } 
+        if((totSum-target)%2==1){
+            return 0;
+        } 
+        
+        int s2 = (totSum-target)/2;
+        
+        return f(n-1,s2,nums);
     }
 };
