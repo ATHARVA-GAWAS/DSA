5ecf47a665875b6f66829cde8c160d327134f180
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-12-28 15:00:32 +0530

    Time: 2 ms (6.33%) | Memory: 10.5 MB (36.62%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,23 +1,28 @@
 class Solution {
-    int f(int ind,vector<int>& nums,vector<int> &dp){
+    int house_robbery(int ind,vector<int>&nums,vector<int>&dp){
         if(ind<0){
             return 0;
         }
+
         if(ind==0){
             return nums[ind];
         }
+
         if(dp[ind]!=-1){
             return dp[ind];
         }
-        int take,nottake;
-        take=nums[ind]+f(ind-2,nums,dp);
-        nottake=nums[ind-1]+f(ind-3,nums,dp);
-        return dp[ind]=max(take,nottake);
+
+        int rob=nums[ind]+house_robbery(ind-2,nums,dp);
+        int dont_rob=nums[ind-1]+house_robbery(ind-3,nums,dp);
+
+        return dp[ind]=max(rob,dont_rob);
     }
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
+
         vector<int> dp(n+1,-1);
-        return f(n-1,nums,dp);
+
+        return house_robbery(n-1,nums,dp);
     }
 };
