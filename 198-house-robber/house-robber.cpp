5454036a7196ed52b1db473aa519180dd22d084e
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-12-28 15:16:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (24.22%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,28 +1,22 @@
 class Solution {
-    int house_robbery(int ind,vector<int>&nums,vector<int>&dp){
-        if(ind<0){
-            return 0;
-        }
-
-        if(ind==0){
-            return nums[ind];
-        }
-
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-
-        int rob=nums[ind]+house_robbery(ind-2,nums,dp);
-        int dont_rob=nums[ind-1]+house_robbery(ind-3,nums,dp);
-
-        return dp[ind]=max(rob,dont_rob);
-    }
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
 
-        vector<int> dp(n+1,-1);
+        if (n == 0) return 0; 
+        if (n == 1) return nums[0];
 
-        return house_robbery(n-1,nums,dp);
+        vector<int> dp(n+1,0);
+
+        dp[0]=nums[0];
+        dp[1] = max(nums[0], nums[1]); // Base case: rob the max of first two houses
+
+        for(int ind=2;ind<n;ind++){
+            int rob=nums[ind]+dp[ind-2];
+            int dont_rob=dp[ind-1];
+
+            dp[ind]=max(rob,dont_rob);
+        }
+        return dp[n-1];
     }
 };
