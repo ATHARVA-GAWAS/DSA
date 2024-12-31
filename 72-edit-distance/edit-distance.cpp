d0ebb9a5215cc5c641f5047bc885d69ca704d44b
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-12-31 22:48:45 +0530

    Time: 4 ms (77.67%) | Memory: 10.5 MB (91.44%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     int minDistance(string word1, string word2) {
         int m = word1.size(), n = word2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        for (int i = 1; i <= m; i++) {
-            dp[i][0] = i;
-        }
+        
+        vector<int> prev(n + 1, 0), cur(n + 1, 0);
+
         for (int j = 1; j <= n; j++) {
-            dp[0][j] = j;
+            prev[j] = j;
         }
+
         for (int i = 1; i <= m; i++) {
+            cur[0] = i;
             for (int j = 1; j <= n; j++) {
                 if (word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    cur[j] = prev[j - 1];
                 } else {
-                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                    cur[j] = min(prev[j - 1], min(cur[j - 1], prev[j])) + 1;
                 }
             }
+            prev=cur;
         }
-        return dp[m][n];
+        return prev[n];
     }
 };
