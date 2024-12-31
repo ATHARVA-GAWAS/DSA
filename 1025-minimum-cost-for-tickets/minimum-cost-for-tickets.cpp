f86fc6b46ef4e46ee96df8c8be5a843b51ae4d35
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-12-31 14:36:05 +0530

    Time: 8 ms (7.53%) | Memory: 23.3 MB (5.11%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    
-    int func(int i, int upto, vector<int>& days, vector<int>& costs, vector<vector<int>>& dp) {
-        if (i == days.size()) return 0;
+    int f(int ind,int upto,vector<int>& days, vector<int>& costs,vector<vector<int>> &dp){
+        if(ind==days.size()){
+            return 0;
+        }
 
-        if (days[i] <= upto) return func(i + 1, upto, days, costs, dp);
+        
 
-        if (dp[i][upto] != -1) return dp[i][upto];
+        if(days[ind]<=upto){
+            return f(ind+1,upto,days,costs,dp);
+        }
 
-        int a = costs[0] + func(i + 1, days[i], days, costs, dp);
-        int b = costs[1] + func(i + 1, days[i] + 6, days, costs, dp);
-        int c = costs[2] + func(i + 1, days[i] + 29, days, costs, dp);
+        if(dp[ind][upto]!=-1){
+            return dp[ind][upto];
+        }
+        
+        int a=costs[0]+f(ind+1,days[ind],days,costs,dp);
+        int b=costs[1]+f(ind+1,days[ind]+6,days,costs,dp);
+        int c=costs[2]+f(ind+1,days[ind]+29,days,costs,dp);
 
-        return dp[i][upto] = min(a, min(b, c));
+        return dp[ind][upto]=min(a,min(b,c));
     }
-    
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<vector<int>> dp(days.size(), vector<int>(366, -1));
-        return func(0, 0, days, costs, dp);
+        int n=days.size();
+
+        vector<vector<int>> dp(n+1,vector<int>(366,-1));
+
+        return f(0, 0, days, costs,dp);
     }
 };
