e7fd3fb0f1b53242c02a52925ea37cd86f82b53c
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-12-31 23:18:24 +0530

    Time: 47 ms (34.62%) | Memory: 48.9 MB (25.72%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,47 +1,33 @@
 class Solution {
-    int f(int ind,int amount,vector<int>&coins,vector<vector<int>> &dp){
-        if(ind<0){
-            return 0;
-        }
-
-        if(ind==0){
-            if(amount%coins[ind]==0){
-                return amount/coins[ind];
-            }
-            else{
-                return INT_MAX;
-            }
-        }
-
-        if(dp[ind][amount]!=-1){
-            return dp[ind][amount];
-        }
-
-        int notpick=0+f(ind-1,amount,coins,dp);
-
-        int pick=INT_MAX;
-
-        if(coins[ind]<=amount){
-            int sub_res = f(ind, amount - coins[ind], coins,dp);
-            if (sub_res != INT_MAX) {  // Prevent overflow
-                pick = 1 + sub_res;
-            }
-        }
-
-        return dp[ind][amount]=min(pick,notpick);
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
 
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
-
-        int ans=f(n-1,amount,coins,dp);
-
-        if(ans>=INT_MAX){
-            return -1;
+        vector<vector<int>> dp(n, vector<int>(amount + 1, INT_MAX));
+        
+        for (int amt = 0; amt <= amount; ++amt) {
+            if (amt % coins[0] == 0) {
+                dp[0][amt] = amt / coins[0];
+            }
         }
 
-        return ans;
+        for (int ind = 1; ind < n; ind++) {
+            for (int amt = 0; amt <= amount; amt++) {
+                int notpick = dp[ind - 1][amt];
+
+                int pick = INT_MAX;
+
+                if (coins[ind] <= amt) { 
+                    int sub_res = dp[ind][amt - coins[ind]];
+                    if (sub_res != INT_MAX) { // Prevent overflow
+                        pick = 1 + sub_res;
+                    }
+                }
+
+                dp[ind][amt] = min(pick, notpick);
+            }
+        }
+
+        return dp[n - 1][amount] == INT_MAX ? -1 : dp[n - 1][amount];
     }
 };
