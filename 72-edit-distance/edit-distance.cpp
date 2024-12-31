3d623b7bde5893c148e60c2a2d471698949b8f9a
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2024-12-31 22:45:28 +0530

    Time: 5 ms (68.66%) | Memory: 13.1 MB (43.56%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,32 +1,23 @@
 class Solution {
-    int f(int ind1,int ind2,string s1,string s2,vector<vector<int>> &dp){
-        if(ind1<0){
-            return ind2+1;
-        }
-        if(ind2<0){
-            return ind1+1;
-        }
-
-        if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
-        }
-
-        if(s1[ind1]==s2[ind2]){
-            return f(ind1-1,ind2-1,s1,s2,dp);
-        }
-
-        int a=1+f(ind1,ind2-1,s1,s2,dp);
-        int b=1+f(ind1-1,ind2,s1,s2,dp);
-        int c=1+f(ind1-1,ind2-1,s1,s2,dp);
-
-        return dp[ind1][ind2]=min(a,min(b,c));
-    }
 public:
     int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m=word2.size();
-
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return f(n-1,m-1,word1,word2,dp);
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
