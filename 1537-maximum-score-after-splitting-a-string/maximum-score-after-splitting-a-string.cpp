e2cf45e110a7daab20ca4117869d2fab822fe1b9
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-01-01 11:22:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (52.60%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int n = s.length();
-        int result = INT_MIN;
-        int total_ones = count(begin(s), end(s), '1');
-        
-        int zeros = 0;
-        
-        for(int i = 0; i < n-1; i++) {
-            if(s[i] == '1') {
-                total_ones--;
-            } else {
-                zeros++;
+        int n=s.size();
+
+        int res = INT_MIN;
+
+        for(int i=0;i<n-1;i++){
+            int left_zero=0;
+            int right_one=0;
+
+            for(int j=0;j<=i;j++){
+                if(s[j]=='0'){
+                    left_zero++;
+                }
             }
-            
-            result = max(result, zeros + total_ones);
+
+            for(int j=i+1;j<n;j++){
+                if(s[j]=='1'){
+                    right_one++;
+                }
+            }
+
+            res=max(res,left_zero+right_one);
         }
-        
-        return result;
+        return res;
     }
 };
