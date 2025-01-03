f52f3dba0d69f44eb9cf19822bf276d12a998efb
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-01-03 22:40:39 +0530

    Time: 4 ms (16.85%) | Memory: 16.5 MB (9.69%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,29 +1,32 @@
 class Solution {
-public:
+    int f(int i,int j,vector<vector<int>>&nums,vector<vector<int>> &dp){
+        int n=nums.size();
+        int m=nums[0].size();
 
+        if(i<0 || j<0){
+            return 1e9;
+        }
+
+        if(i==0 && j==0){
+            return nums[i][j];
+        }
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int left=nums[i][j]+f(i,j-1,nums,dp);
+        int up=nums[i][j]+f(i-1,j,nums,dp);
+
+        return dp[i][j]=min(left,up);        
+    }
+public:
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<int> prev(n+1,0),cur(m+1,0);
-    for(int i=0; i<n ; i++){
-        for(int j=0; j<m; j++){
-            if(i==0 && j==0) cur[j] = grid[i][j];
-            else{
-                
-                int up = grid[i][j];
-                if(i>0) up += prev[j];
-                else up += 1e9;
-                
-                int left = grid[i][j];
-                if(j>0) left+=cur[j-1];
-                else left += 1e9;
-                
-                cur[j] = min(up,left);
-            }
-        }
-        prev=cur;
-    }
-    
-    return prev[m-1];
+
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        
+        return f(n-1,m-1,grid,dp);
     }
 };
