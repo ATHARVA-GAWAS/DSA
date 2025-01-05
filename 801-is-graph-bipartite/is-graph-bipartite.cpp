65dba16dbc8ee66b9894de9acc4a74901267c3ff
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-01-05 22:27:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (55.94%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -2,26 +2,27 @@ class Solution {
 private:
     bool isSameColored(int node,int color,vector<int> &vis,vector<vector<int>> &graph){
         vis[node]=color;
+
         //neighbours
-        for(auto it:graph[node]){
-            if(vis[it]==-1){
-                if(isSameColored(it,!color,vis,graph)==false){
+        for(auto neighbour:graph[node]){
+            if(vis[neighbour]==-1){
+                if(isSameColored(neighbour,!color,vis,graph)==false){
                     return false;
                 }
             }
-            else if(vis[it]==color){
+            else if(vis[neighbour]==color){
                 return false;
             }
         }
         return true;
-        
     }
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        //graph is the adj list
+
         int n=graph.size();
+        
         vector<int> vis(n,-1);
-        //multisource as per point 4 in the graph properties
+
         for(int i=0;i<n;i++){
             if(vis[i]==-1){
                 if(isSameColored(i,0,vis,graph)==false){
@@ -29,8 +30,7 @@ public:
                 }
             }
         }
+
         return  true;
-        
-        
     }
 };
