6aa58e95f2aa06b9e99703d3b9dd349333aa8e95
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-01-30 14:59:19 +0530

    Time: 163 ms (41.18%) | Memory: 41.9 MB (88.66%) - LeetSync

---
+++ 2583-divide-nodes-into-the-maximum-number-of-groups/divide-nodes-into-the-maximum-number-of-groups.cpp
@@ -0,0 +1,106 @@
+class Solution {
+public:
+    //1 : Red
+    //0 : Green
+    bool isBipartite(unordered_map<int, vector<int>> &adj, int curr, vector<int>& colors, int currColor) {
+        colors[curr] = currColor;
+
+        for(int &ngbr : adj[curr]) {
+            if(colors[ngbr] == colors[curr]) {
+                return false;
+            }
+
+            if(colors[ngbr] == -1) {
+                if(isBipartite(adj, ngbr, colors, 1 - currColor) == false) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    int bfs(unordered_map<int, vector<int>> &adj, int currNode, int n) {
+        queue<int> que;
+        vector<bool> visited(n, false);
+        que.push(currNode);
+        visited[currNode] = true;
+
+        int level = 1; //max groups in that components
+        
+        while(!que.empty()) {
+
+            int size = que.size();
+            while(size--) {
+                int curr = que.front();
+                que.pop();
+
+                for(int &ngbr : adj[curr]) {
+                    if(visited[ngbr])
+                        continue;
+                    
+                    que.push(ngbr);
+                    visited[ngbr] = true;
+                }
+            }
+            level++; //1 extra will be incremented in the last loop
+        }
+
+        return level-1;
+    }
+
+    int getMaxFromEachComp(unordered_map<int, vector<int>> &adj, int curr, vector<bool>& visited, vector<int>& levels) {
+        int maxGrp = levels[curr];
+        visited[curr] = true;
+
+        for(int &ngbr : adj[curr]) {
+            if(!visited[ngbr]) {
+                maxGrp = max(maxGrp, getMaxFromEachComp(adj, ngbr, visited, levels));
+            }
+        }
+
+        return maxGrp;
+    }
+
+    int magnificentSets(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+
+        for(auto& edge : edges) {
+            int u = edge[0] - 1; //converting to 0-based idnex
+            int v = edge[1] - 1;
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        //Bipartite check
+        vector<int> colors(n, -1);
+
+        for(int node = 0; node < n; node++) { 
+            if(colors[node] == -1) {
+                if(isBipartite(adj, node, colors, 1) == false) { 
+                    return -1;
+                }
+            }
+        }
+
+        //BFS- max levels for each node
+        vector<int> levels(n, 0);
+
+        for(int node = 0; node < n; node++) { 
+            levels[node] = bfs(adj, node, n);
+        }
+
+
+        int maxGroupEachComp = 0;
+        vector<bool> visited(n, false);
+
+        for(int node = 0; node < n; node++) { 
+            if(!visited[node]) {
+                maxGroupEachComp += getMaxFromEachComp(adj, node, visited, levels);
+            }
+        }
+
+        return maxGroupEachComp;
+    }
+};
