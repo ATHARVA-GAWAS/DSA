f5627bafd802e2b9bdb1806595b6cdd01b5b9236
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-02-01 08:49:03 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (61.71%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -1,23 +1,10 @@
 class Solution {
-    bool is_even(int a){
-        if(a%2==0){
-            return true;
-        }
-        return false;
-    }
-
-    bool is_odd(int a){
-        if(a%2!=0){
-            return true;
-        }
-        return false;
-    }
 public:
     bool isArraySpecial(vector<int>& nums) {
         int n=nums.size();
 
         for(int i=0;i<n-1;i++){
-            if(is_odd(nums[i]) && is_odd(nums[i+1]) || is_even(nums[i]) && is_even(nums[i+1])){
+            if(((nums[i]^nums[i+1])&1)==0){
                 return false;
             }
         }
