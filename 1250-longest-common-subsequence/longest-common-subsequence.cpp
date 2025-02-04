5accad6f69ad5dc7ac39f409dc7c780fd3fa3941
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-02-04 10:23:43 +0530

    Time: 27 ms (56.46%) | Memory: 27.3 MB (66.52%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,36 +1,48 @@
 class Solution {
-    int f(int ind1,int ind2,string text1, string text2,vector<vector<int>> &dp){
+    int f(int ind1,int ind2,string a, string b,vector<vector<int>> &dp){
         if(ind1<0 || ind2<0){
             return 0;
         }
+
         if(dp[ind1][ind2]!=-1){
             return dp[ind1][ind2];
         }
-        int take=0,nottake1=0,nottake2=0;
-        if(text1[ind1]==text2[ind2]){
-            take=1+f(ind1-1,ind2-1,text1,text2,dp);
+
+        int x=0,y=0,z=0;
+
+        if(a[ind1]==b[ind2]){
+            x=1+f(ind1-1,ind2-1,a,b,dp);
         }
         else{
-            nottake1=0+f(ind1-1,ind2,text1,text2,dp);
-            nottake2=0+f(ind1,ind2-1,text1,text2,dp);
+            y=f(ind1-1,ind2,a,b,dp);
+            z=f(ind1,ind2-1,a,b,dp);
         }
-        return dp[ind1][ind2]=max(take,max(nottake1,nottake2));
+
+        return dp[ind1][ind2]=max(x,max(y,z));
     }
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.size();
-        int m=text2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(text1[i-1]==text2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+        int n1=text1.size();
+        int n2=text2.size();
+
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+
+        for(int ind1=1;ind1<=n1;ind1++){
+            for(int ind2=1;ind2<=n2;ind2++){
+                int x=0,y=0,z=0;
+
+                if(text1[ind1-1]==text2[ind2-1]){
+                    x=1+dp[ind1-1][ind2-1];
                 }
                 else{
-                   dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                    y=dp[ind1-1][ind2];
+                    z=dp[ind1][ind2-1];
                 }
+
+                dp[ind1][ind2]=max(x,max(y,z));
             }
         }
-        return dp[n][m];
+        
+        return dp[n1][n2];
     }
 };
