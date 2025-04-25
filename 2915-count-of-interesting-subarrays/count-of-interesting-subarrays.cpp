a5497c3eed6adb608ee5fa5cd5ab22d9762adaf2
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-04-25 20:58:32 +0530

    Time: 65 ms (34.43%) | Memory: 126.8 MB (20.22%) - LeetSync

---
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+        long long count = 0, equals = 0;
+
+        unordered_map<int, long long> mpp;
+
+        mpp[0] = 1;
+
+        for (int it : nums) {
+            if (it % modulo == k){
+                equals++;
+            } 
+
+            int rem = equals % modulo;
+
+            int needed = (rem - k + modulo) % modulo;
+
+            count += mpp[needed];
+
+            mpp[rem]++;
+        }
+
+        return count;
+    }
+};
