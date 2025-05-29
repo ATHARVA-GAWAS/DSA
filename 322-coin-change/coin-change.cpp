4e69c4004c900818e5e64b5fb444d12535028a8d
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-05-29 09:43:29 +0530

    Time: 50 ms (26.71%) | Memory: 53.6 MB (10.74%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,34 +1,47 @@
 class Solution {
+    int f(int ind,int amount,vector<int>&coins,vector<vector<int>> &dp){
+        if(ind<0){
+            return 0;
+        }
+
+        if(ind==0){
+            if(amount%coins[ind]==0){
+                return amount/coins[ind];
+            }
+            else{
+                return INT_MAX;
+            }
+        }
+
+        if(dp[ind][amount]!=-1){
+            return dp[ind][amount];
+        }
+
+        int notpick=0+f(ind-1,amount,coins,dp);
+
+        int pick=INT_MAX;
+
+        if(coins[ind]<=amount){
+            int sub_res = f(ind, amount - coins[ind], coins,dp);
+            if (sub_res != INT_MAX) {  // Prevent overflow
+                pick = 1 + sub_res;
+            }
+        }
+
+        return dp[ind][amount]=min(pick,notpick);
+    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
 
-        vector<int> prev(amount + 1, INT_MAX),cur(amount + 1, INT_MAX);
-        
-        for (int amt = 0; amt <= amount; ++amt) {
-            if (amt % coins[0] == 0) {
-                prev[amt] = amt / coins[0];
-            }
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+
+        int ans=f(n-1,amount,coins,dp);
+
+        if(ans>=INT_MAX){
+            return -1;
         }
 
-        for (int ind = 1; ind < n; ind++) {
-            for (int amt = 0; amt <= amount; amt++) {
-                int notpick = prev[amt];
-
-                int pick = INT_MAX;
-
-                if (coins[ind] <= amt) { 
-                    int sub_res = cur[amt - coins[ind]];
-                    if (sub_res != INT_MAX) { // Prevent overflow
-                        pick = 1 + sub_res;
-                    }
-                }
-
-                cur[amt] = min(pick, notpick);
-            }
-            prev=cur;
-        }
-
-        return prev[amount] == INT_MAX ? -1 :prev[amount];
+        return ans;
     }
 };
