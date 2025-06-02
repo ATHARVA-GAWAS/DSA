8dec6a0ecb31b99c44da53898b4b319feb596f4e
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-06-02 10:29:31 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (17.97%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        int sum=1;
-        int i=1;
-        while(i<n){
-            if(ratings[i]==ratings[i-1]){
-                sum++;
-                i++;
-            }
-            int peak=1;
-            while(i<n && ratings[i]>ratings[i-1]){
-                peak++;
-                sum+=peak;
-                i++;
-            }
-            int down=1;
-            while(i<n && ratings[i]<ratings[i-1]){
-                sum+=down;
-                i++;
-                down++;
-            }
-            if(down>peak){
-                sum+=down-peak;
-            }
+        
+        vector<int> left(n, 1);
+        vector<int> right(n, 1);
+        
+        //First comparing with only left neighbour from left to rigth direction in the array
+        for(int i = 1; i<n; i++) {
+            if(ratings[i] > ratings[i-1]){
+                left[i] = max(left[i], left[i-1]+1);
+            }   
         }
-        return sum;
+        
+        //Then comparing with only right neighbour from right to left direction in the array
+        for(int i = n-2; i>=0; i--) {
+            if(ratings[i] > ratings[i+1]){
+                right[i] = max(right[i], right[i+1]+1);
+            }  
+        }
+        
+        
+        int result = 0;
+        
+        for(int i = 0; i<n; i++) {
+            result += max(left[i], right[i]);
+        }
+        
+        return result;
     }
 };
