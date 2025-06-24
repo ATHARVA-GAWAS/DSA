e8752421fa07595ba12a9d5214f574b8c18d9f6a
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-06-24 20:22:15 +0530

    Time: 123 ms (19.45%) | Memory: 16 MB (27.71%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
@@ -9,7 +9,7 @@ public:
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if( abs(i-j)<=k && nums[j]==key){
+                if( st.find(i)==st.end() && abs(i-j)<=k && nums[j]==key){
                     st.insert(i);
                 }
             }
