c0f0458bc73d0cd4ff55e9c81e1359e21c689d05
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-06-26 19:37:33 +0530

    Time: 840 ms (22.96%) | Memory: 11.9 MB (13.33%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,40 +1,48 @@
 class Solution {
-public:
-    bool search(int i, int j, int n, int m, vector<vector<char>>& board, int k, string word) {
-        if (k == word.size()) return true;  // If we have matched all characters in the word, return true
-        if (i == n || i < 0 || j < 0 || j == m || board[i][j] != word[k]) {
-            return false;  // If current cell is out of bounds or does not match the current character in word, return false
+    bool dfs(int i,int j,vector<vector<int>> &vis,vector<vector<char>>& board, int k, string word){
+        int n=board.size();
+        int m=board[0].size();
+
+        if (k == word.size()) return true;
+
+        if(i<0 || j<0 || i>=n || j>=m || vis[i][j]==1){
+            return false;
         }
-        
-        char ch = board[i][j];  // Store the current character before exploring neighbors
-        board[i][j] = '#';  // Mark the current cell as visited to avoid revisiting it
-        
-       
-        bool op1 = search(i + 1, j, n, m, board, k + 1, word);  // Down
-        bool op2 = search(i - 1, j, n, m, board, k + 1, word);  // Up
-        bool op3 = search(i, j + 1, n, m, board, k + 1, word);  // Right
-        bool op4 = search(i, j - 1, n, m, board, k + 1, word);  // Left
-        
-        board[i][j] = ch;  // Restore the original character in the board
-        
-        return op1 || op2 || op3 || op4;  // Return true if any of the recursive calls return true
+
+        if(board[i][j]!=word[k]){
+            return false;
+        }
+
+        vis[i][j]=1;
+
+        bool a=dfs(i-1,j,vis,board,k+1,word);
+        bool b=dfs(i,j-1,vis,board,k+1,word);
+        bool c=dfs(i+1,j,vis,board,k+1,word);
+        bool d=dfs(i,j+1,vis,board,k+1,word);
+
+        vis[i][j]=0;
+
+        return a||b||c||d;
+
     }
-
+public:
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size(); 
-        int m = board[0].size();  
-        int k = word.size();  
+        int n=board.size();
+        int m=board[0].size();
 
-        // Iterate over each cell in the board
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                // Check if the word can be formed starting from the current cell
-                if (search(i, j, n, m, board, 0, word)) {
-                    return true;  // If found, return true
+        vector<vector<int>> vis(n,vector<int>(m,-1));
+
+        bool ans;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                ans=dfs(i,j,vis,board,0,word);
+                if(ans==true){
+                    return true;
                 }
             }
         }
 
-        return false;  // If no match is found, return false
+        return false;
     }
 };
