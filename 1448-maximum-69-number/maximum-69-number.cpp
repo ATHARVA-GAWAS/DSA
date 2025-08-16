2b07ce5328bebe61eafd3bc789aaef49ef6134f6
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-08-16 09:56:26 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (71.54%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        if(num==9999){
+            return 9999;
+        }
+
+        string n=to_string(num);
+
+        for(int i=0;i<n.size();i++){
+            if(n[i]=='6'){
+                n[i]='9';
+
+                break;
+            }
+        }
+
+        cout<<n;
+
+        return stoi(n);
+    }
+};
