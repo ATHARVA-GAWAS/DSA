5f10364898b42a5beb2919e5c85f82e111621c16
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-08-20 20:00:27 +0530

    Time: 8 ms (44.13%) | Memory: 30.4 MB (21.64%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int f(int i,int j, vector<vector<int>>&matrix,vector<vector<int>> &dp){
+    int f(int i,int j,vector<vector<int>>&matrix,vector<vector<int>> &dp){
         if(i<0 || j<0 || matrix[i][j]==0){
             return 0;
         }
@@ -9,8 +9,8 @@ class Solution {
         }
 
         int a=f(i-1,j,matrix,dp);
-        int b=f(i-1,j-1,matrix,dp);
-        int c=f(i,j-1,matrix,dp);
+        int b=f(i,j-1,matrix,dp);
+        int c=f(i-1,j-1,matrix,dp);
 
         return dp[i][j]=1+min(a,min(b,c));
     }
@@ -21,7 +21,8 @@ public:
 
         int cnt=0;
 
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        vector<vector<int>> dp(n,vector<int>(m+1,-1));
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 cnt=cnt+f(i,j,matrix,dp);
