323573d25418852d6b7d2ee407dbbc5f152b80d9
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2025-11-08 19:42:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (55.27%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -1,11 +1,13 @@
-
 class Solution {
 public:
     int minimumOneBitOperations(int n) {
-        if (n==0) return 0;//base case
-        int k=floor(log2(n));//31-__builtin_clz(n);
-    //    cout<<(1<<k)<<endl;
-        int numOp=(1<<(k+1))-1;
-        return numOp-minimumOneBitOperations(n^(1<<k));
+        int mask=n;
+
+        while(mask){
+            mask>>=1;
+            n^=mask;
+        }
+
+        return n;
     }
 };
