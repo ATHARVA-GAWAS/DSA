31f0e20986efbaebb151d83f11d1b148aaf12057
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2026-01-01 15:39:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (92.86%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        int carry = 1;
+
+        vector<int> ans;
+
+        for (int i=n-1;i>=0;i--) {
+            int sum = digits[i]+carry;
+
+            ans.push_back(sum % 10);
+
+            carry=sum/10;
+        }
+
+        if (carry) {
+            ans.push_back(carry);
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
