fa3b97fc6d4cae2d9e25a6b46be33e4176b2a515
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2026-01-10 19:25:54 +0530

    Time: 35 ms (28.95%) | Memory: 18.5 MB (9.87%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,31 +1,43 @@
 class Solution {
-public:
-    int minimumDeleteSum(string s1, string s2) {
-    int n = s1.length();
-    int m = s2.length();
-    
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-    
-    // Initialize the first row and column
-    for (int i = 1; i <= n; ++i) {
-        dp[i][0] = dp[i - 1][0] + s1[i - 1]; 
-    }
-    for (int j = 1; j <= m; ++j) {
-        dp[0][j] = dp[0][j - 1] + s2[j - 1]; 
-    }
+    int lcsAscii(int i, int j, string &s1, string &s2,vector<vector<int>> &dp) {
+        if (i == s1.length() || j == s2.length()){
+            return 0;
+        }
 
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+            
 
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= m; ++j) {
-            if (s1[i - 1] == s2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1];
-            } else {
-                dp[i][j] = min(dp[i - 1][j] + s1[i - 1],  // Delete from s1
-                               dp[i][j - 1] + s2[j - 1]); // Delete from s2
-            }
+        if (s1[i] == s2[j]) {
+            return dp[i][j]=s1[i]+lcsAscii(i+1,j+1,s1,s2,dp);
+        } 
+        else {
+            return dp[i][j]=max(
+                lcsAscii(i+1,j,s1,s2,dp),
+                lcsAscii(i,j+1,s1,s2,dp)
+            );
         }
     }
 
-    return dp[n][m];
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        int n=s1.size();
+        int m=s2.size();
+
+        int total = 0;
+
+        for (char c : s1){
+            total += c;
+        } 
+        for (char c : s2){
+            total += c;
+        } 
+
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+
+        int common = lcsAscii(0, 0, s1, s2,dp);
+
+        return total-2*common;
     }
 };
