1bf98f29261032c9617ec14583670b6974f20fd6
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2026-02-15 13:44:22 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (97.06%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,49 +1,37 @@
 class Solution {
 public:
+    void dfs(vector<vector<int>>& grid, int i, int j, int m, int n, int minTime) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0 || (grid[i][j] > 1 && grid[i][j] < minTime)){
+            return;
+        }
+            
+        grid[i][j] = minTime;
+        
+        dfs(grid, i - 1, j, m, n, minTime + 1);
+        dfs(grid, i + 1, j, m, n, minTime + 1);
+        dfs(grid, i, j - 1, m, n, minTime + 1);
+        dfs(grid, i, j + 1, m, n, minTime + 1);
+    }
+
     int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-         queue<pair<pair<int,int>,int>> q;
-    int vis[n][m];
-    int cntFresh=0;
-    int cnt=0;
-    //{{r,c},t}
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(grid[i][j]==2){
-              q.push({{i,j},0});
-              vis[i][j]=2;
-            }
-            else{
-                vis[i][j]=0;
-            }
-            if(grid[i][j]==1){
-                cntFresh++;
+        int m = grid.size(), n = grid[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2)
+                    dfs(grid, i, j, m, n, 2);
             }
         }
-    }
-    int tm=0;
-    int drow[]={-1,0,1,0};
-    int dcol[]={0,1,0,-1};
-    while(!q.empty()){
-        int r=q.front().first.first;
-        int c=q.front().first.second;
-        int t=q.front().second;
-        tm=max(tm,t);
-        q.pop();
-        for(int i=0;i<4;i++){
-            int nrow=r+drow[i];
-            int ncol=c+dcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
-                q.push({{nrow,ncol},t+1});
-                vis[nrow][ncol]=2;
-                cnt++;
+        
+        int minutes = 2;
+
+        for (auto& row : grid) {
+            for (int cell : row) {
+                if (cell == 1) return -1;
+                minutes = max(minutes, cell);
             }
         }
-    }
-    if(cnt!=cntFresh){
-        return -1;
-    }
-    return tm;
+
+        return minutes - 2;
     }
 };
