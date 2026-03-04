7817ff9ed6b0825758fdb1167c3ffa5007833bb8
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2026-03-04 20:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (85.69%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -4,23 +4,33 @@ public:
         int result=0;
         int m=mat.size();
         int n=mat[0].size();
-        vector<int> rowCount(m,0);
-        vector<int> colCount(n,0);
+        
         for(int row=0;row<m;row++){
             for(int col=0;col<n;col++){
-                if(mat[row][col]==1){
-                    rowCount[row]++;
-                    colCount[col]++;
+                if(mat[row][col]==0){
+                    continue;
                 }
-            }
-        }
-        for(int row=0;row<m;row++){
-            for(int col=0;col<n;col++){
-                 if(mat[row][col]==1){
-                     if(rowCount[row]==1 && colCount[col]==1){
-                         result++;
-                     }
-                 }
+                bool special=true;
+                //check row
+
+                for(int r=0;r<m;r++){
+                    if(r!=row && mat[r][col]==1){
+                        special=false;
+                        break;
+                    }
+                }
+
+                //check col
+
+                for(int c=0;c<n;c++){
+                    if(c!=col && mat[row][c]==1){
+                        special=false;
+                        break;
+                    }
+                }
+                if(special){
+                    result++;
+                }
             }
         }
         return result;
