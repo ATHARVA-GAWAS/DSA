363e3ea154ae9bec34089be1cf2b98920888603a
ATHARVA-GAWAS <111885892+ATHARVA-GAWAS@users.noreply.github.com>
2026-03-13 22:14:48 +0530

    Time: 174 ms (18.06%) | Memory: 34.5 MB (8.39%) - LeetSync

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,51 +1,30 @@
 class Solution {
-    bool f(int mountainHeight,const vector<int> &workerTimes,long long maxTime){
-        long long tot=0;
-
-        for(int time:workerTimes){
-            long long l=0,h=mountainHeight, best=0;
-
-            while(l<=h){
-                long long mid=l+(h-l)/2;
-
-                long long req=time*(mid*(mid+1))/2;
-
-                if(req<=maxTime){
-                    best=mid;
-                    l=mid+1;
-                }
-                else{
-                    h=mid-1;
-                }
-            }
-
-            tot+=best;
-
-            if(tot>=mountainHeight){
-                return true;
-            }
-        }
-
-        return tot>=mountainHeight;
-    }
 public:
     long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
-        long long l=0,h=LLONG_MAX;
+        using ll = long long;
+        
+        priority_queue<
+            tuple<ll,ll,ll>,
+            vector<tuple<ll,ll,ll>>,
+            greater<>
+        > pq;
 
-        long long res=LLONG_MAX;
-
-        while(l<=h){
-            long long m=l+(h-l)/2;
-
-            if(f(mountainHeight, workerTimes, m)){
-                res=m;
-                h=m-1;
-            }
-            else{
-                l=m+1;
-            }
+        for(int t : workerTimes) {
+            pq.push({t,1,t});  
+            // {finishTime, nextMultiplier, baseTime}
         }
 
-        return res;
+        ll ans = 0;
+
+        for(int i=0;i<mountainHeight;i++) {
+            auto [time,k,t] = pq.top();
+            pq.pop();
+
+            ans = max(ans,time);
+
+            pq.push({time + (ll)t*(k+1), k+1, t});
+        }
+
+        return ans;
     }
 };
